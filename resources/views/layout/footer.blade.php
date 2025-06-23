9c51ebc426dfba3eb7c7912bf3f90b4ee057abc2
MKashanButt <kashanbutt566@gmail.com>
2025-06-24 02:38:13 +0500

    fixed footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="flex items-center justify-center bg-black text-white py-16">
-    <div class="w-[90%] flex flex-wrap gap-8 md:gap-4">
+    <div class="w-[90%] flex lg:flex-nowrap gap-8 md:gap-4">
         <!-- About Company -->
         <div class="w-full sm:w-1/2 md:w-1/4 flex flex-col gap-4">
             <h3 class="font-bold tracking-wide text-2xl uppercase">About Company</h3>
@@ -37,17 +37,17 @@
         <div class="w-full sm:w-1/2 md:w-1/4 flex flex-col gap-4">
             <h3 class="font-bold tracking-wide text-2xl uppercase">Socials</h3>
             <div class="flex gap-4">
-                <span class="cursor-pointer hover:scale-110 transition">
+                <span class="cursor-pointer">
                     <!-- Facebook Icon -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" stroke="currentColor"
                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                     </svg>
                 </span>
-                <span class="cursor-pointer hover:scale-110 transition">
+                <span class="cursor-pointer">
                     <!-- Instagram Icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" stroke="currentColor"
-                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
                         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
                         <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
